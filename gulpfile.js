8a7fe4197104542759d3d07c38572c1588a17125
Eduardo Dias da Costa <eduardo@thelaborat.org>
2016-03-20 20:34:23 -0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,47 @@
-var gulp    = require("gulp");
-var del     = require("del");
-var sass    = require("gulp-sass");
-var rename  = require("gulp-rename");
+var gulp     = require("gulp");
+var del      = require("del");
+var sass     = require("gulp-sass");
+var rename   = require("gulp-rename");
+var template = require("gulp-template");
 
 var isDebug = false;
 
 console.log("gulp> Init.");
 
-/**
- * Main Paths
- */
+//Main Paths
 var path = {
-    src: "source/",
-    dst: "deploy/"    
+    src: "./source",
+    dst: "./deploy"    
 };
 
-//Path to js files
-path.js   = { src: path.src+"js/", dst: path.dst+"js/" };
+//File Paths
+path.js   = { src: path.src+"/js",   dst: path.dst+"/js" };
+path.scss = { src: path.src+"/scss", dst: path.dst+"/css" };
+path.html = { src: path.src+"/html", dst: path.dst };
 
-//Path to scss files
-path.scss = { src: path.src+"scss/", dst: path.dst+"css/" };
+//Source Streams
+var jsFiles   = gulp.src(path.js.src  +"/**/*.js");
+var scssFiles = gulp.src(path.scss.src+"/**/*.scss");
+var htmlFiles = gulp.src(path.html.src+"/**/*.html");
 
-//Source Stream for JS
-var jsFiles = gulp.src(path.js.src+"**/*.js");
-
-//Destination Stream for JS
+//Destination Streams
 var jsDst   = gulp.dest(path.js.dst);
+var scssDst = gulp.dest(path.scss.dst);
+var htmlDst = gulp.dest(path.html.dst);
 
-//Source stream for Sass
-var scssFiles = gulp.src(path.scss.src+"**/*.scss");
-
-//Destination stream for Sass
-var scssDst   = gulp.dest(path.scss.dst);
-
+//Delete the contents of deploy folder
+gulp.task("clean",function clean(){
+    return del(path.dst);
+})
 
 //Move Javascript files to the deploy folder.
 gulp.task("move-js", function moveJS() {    
     return jsFiles.pipe(jsDst);
+});
+
+//Move HTML files to the deploy folder.
+gulp.task("move-html", function moveHTML() {    
+    return htmlFiles.pipe(htmlDst);
 });
 
 //Build Sass files to CSS deploy folder.
@@ -53,7 +58,17 @@ gulp.task("build-scss",function buildScss() {
             
 });
 
+//Build Template files to HTML deploy folder.
+gulp.task("build-html",function buildScss() {
+    
+    var htmlConfig = require(path.html.src+"/config.json");    
+    
+    htmlFiles
+    .pipe(template(htmlConfig))
+    .pipe(htmlDst);
+    
+});
 
 //Builds the website.
-gulp.task("build",["move-js","build-scss"],function build() { });
+gulp.task("build",["move-js","build-html","build-scss"],function build() { });
 
